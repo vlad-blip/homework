2654240cb40305180b0dcd4577aeb836d6512906
Vladyslav Lazarenko <ezio2506@gmail.com>
2022-05-20 00:50:22 +0300

    final push

--- webpack.config.js
+++ webpack.config.js
@@ -117,6 +117,11 @@ module.exports = {
             ignore: ['*.DS_Store', 'Thumbs.db'],
           },
         },
+        {
+          from: path.resolve(environment.paths.source, 'videos'),
+          to: path.resolve(environment.paths.output, 'videos'),
+          toType: 'dir',
+        },
       ],
     }),
   ].concat(htmlPluginEntries),
